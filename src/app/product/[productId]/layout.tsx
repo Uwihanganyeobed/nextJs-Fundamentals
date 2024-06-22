5074161119cf92a6d56364f516f382b0b11d25f8
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-06-22 20:05:32 +0200

    nested-layouts

---
+++ src/app/product/[productId]/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>
+  {children}
+  <h2>Features products</h2>
+  {/* corousel here */}
+  </>;
+}
